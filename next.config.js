fc5b0c319868d25311d4aecc5b5d6dd110b7bf94
임승현 <96limshyun@gmail.com>
2024-08-23 23:31:49 +0900

    fix: 정적 사이트 생성을 위한 next export 추가
    - GitHub Pages 배포를 위해 next build 후 next export 명령 추가
    - 정적 HTML 파일이 ./out 디렉토리에 저장되도록 설정
    - Next.js 사이트의 정적 배포를 위한 워크플로우 업데이트

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const { withContentlayer } = require("next-contentlayer");
 
+const prefix =
+    process.env.NODE_ENV === "production"
+        ? "https://meetyourbook.github.io/"
+        : "";
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
+    output: "export",
+    basePath: prefix,
+    assetPrefix: prefix,
 };
 
 module.exports = withContentlayer(nextConfig);
